64eb7f3c816b1cde0b0742b580c9e1457c4f4806
Danil Demin <prodbykngns@gmail.com>
2024-06-30 20:24:42 +0300

    Added animated AmmoBar

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -9,25 +9,80 @@ public class Weapon : MonoBehaviour
     [SerializeField] private PlayerController playerController;
     private Camera mainCam;
     private Vector3 mousePos;
-    private int maxWaterAmmo = 1000;
+    private int maxWaterAmmo = 600;
     private int currentWaterAmmo;
-    [SerializeField] private AmmoText ammoBar;
+    [SerializeField] private Animator animator;
+    [SerializeField] private GameObject ammoBar;
     [SerializeField] private GameObject waterParticle;
 
     private void Start()
     {
         mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
         currentWaterAmmo = maxWaterAmmo;
-        ammoBar.SetMaxWaterAmmo(maxWaterAmmo);
+        animator = ammoBar.GetComponent<Animator>();
     }
 
     void Update()
     {
-        ammoBar.UpdateAmmoText(currentWaterAmmo);
 
         mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
 
         Vector3 rotation = mousePos - transform.position;
+
+        if (currentWaterAmmo > 500 && currentWaterAmmo < 550)
+        {
+            animator.SetInteger("state", 1);
+        }
+        else if (currentWaterAmmo > 450 && currentWaterAmmo < 500)
+        {
+            animator.SetInteger("state", 2);
+        }
+        else if (currentWaterAmmo > 400 && currentWaterAmmo < 450)
+        {
+            animator.SetInteger("state", 3);
+        }
+        else if (currentWaterAmmo > 350 && currentWaterAmmo < 400)
+        {
+            animator.SetInteger("state", 4);
+        }
+        else if (currentWaterAmmo > 300 && currentWaterAmmo < 350)
+        {
+            animator.SetInteger("state", 5);
+        }
+        else if (currentWaterAmmo > 250 && currentWaterAmmo < 300)
+        {
+            animator.SetInteger("state", 6);
+        }
+        else if (currentWaterAmmo > 200 && currentWaterAmmo < 250)
+        {
+            animator.SetInteger("state", 7);
+        }
+        else if (currentWaterAmmo > 150 && currentWaterAmmo < 200)
+        {
+            animator.SetInteger("state", 8);
+        }
+        else if (currentWaterAmmo > 100 && currentWaterAmmo < 150)
+        {
+            animator.SetInteger("state", 9);
+        }
+        else if (currentWaterAmmo > 75 && currentWaterAmmo < 100)
+        {
+            animator.SetInteger("state", 10);
+        }
+        else if (currentWaterAmmo > 50 && currentWaterAmmo < 75)
+        {
+            animator.SetInteger("state", 11);
+        }
+        else if (currentWaterAmmo > 0 && currentWaterAmmo < 50)
+        {
+            animator.SetInteger("state", 12);
+        }
+        else if (currentWaterAmmo <= 0)
+        {
+            animator.SetInteger("state", 13);
+        }
+
+        Debug.Log(currentWaterAmmo);
 
         float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
         if (playerController.GetIsFacingRight())
