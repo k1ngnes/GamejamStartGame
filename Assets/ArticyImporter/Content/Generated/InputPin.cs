fbca84004154e89f9ade2e8b448071ac92662246
Petr0vDaniil <petr.dan.02@gmail.com>
2024-07-01 03:42:59 +0300

    PrePull

--- Assets/ArticyImporter/Content/Generated/InputPin.cs
+++ Assets/ArticyImporter/Content/Generated/InputPin.cs
@@ -22,7 +22,7 @@ namespace Articy.Jam
     
     
     [Serializable()]
-    [Articy.Unity.ArticyCodeGenerationHashAttribute(638553971812025638)]
+    [Articy.Unity.ArticyCodeGenerationHashAttribute(638553993744572909)]
     public class InputPin : ArticyPrimitive, IInputPin
     {
         
